8ce06654d59dd85ba03bd147549d2272bc5031b5
danibetzamora <danielbetancor2000@gmail.com>
2022-04-26 23:22:48 +0100

    Chat modificado y estilos aplicados - Primera versión de chat

--- chatLoad.php
+++ chatLoad.php
@@ -15,17 +15,23 @@
             (chat.usuario_uno = ".$user[0]." OR chat.usuario_dos = ".$user[0].") AND 
             (chat.usuario_uno = ".$_SESSION['user_dos']." OR chat.usuario_dos = ".$_SESSION['user_dos'].") WHERE mensaje.chat = chat.id;";*/
 
-        $sql = "SELECT * FROM mensaje JOIN chat ON (chat.usuario_uno = ".$user[0]." OR chat.usuario_dos = ".$user[0].") WHERE mensaje.chat = chat.id;";
+        $sql = "SELECT * FROM mensaje JOIN chat ON (chat.usuario_uno = ".$user[0]." OR chat.usuario_dos = ".$user[0].") WHERE mensaje.chat = chat.id ORDER BY mensaje.date ASC;";
 
         $ejecutar = $connection -> query($sql);
 
         while ($row = $ejecutar -> fetch_array()):
             if($row['chat'] == $_SESSION['chat_id']){
-        ?>
-                <div id="datos-chat">
-                    <span><?php echo $row['texto']; ?></span>
-                    <span><?php echo $row['date']; ?></span>
-                </div>
+                if($row['emisor'] == $_SESSION['user']['id']){?>
+                    <div id="datos-chat" style="display:flex; justify-content:flex-end; width:98%; height:10%; gap:2rem; align-items: center;">
+                        <span style="background-color:#AEF67D; height:50%; display:flex; align-items: center; font-weight: bold; padding: 0.3em; border: 2px solid #EFEFEF; border-radius: 8px;"><?php echo $row['texto']; ?></span>
+                        <span style="height:50%; display:flex; align-items: center; font-size: small;"><?php echo $row['date']; ?></span>
+                    </div>
+                <?php }else{ ?>
+                    <div id="datos-chat" style="display:flex; justify-content:flex-start; width:95%; height:10%; gap:2rem; align-items: center; margin-left: 1em;">
+                        <span style="background-color:#FFFFFF; height:50%; display:flex; align-items: center; font-weight: bold; padding: 0.3em; border: 2px solid #EFEFEF; border-radius: 8px;"><?php echo $row['texto']; ?></span>
+                        <span style="height:50%; display:flex; align-items: center; font-size: small;"><?php echo $row['date']; ?></span>
+                    </div>
+            <?php } ?>
         <?php
             }
             endwhile;
